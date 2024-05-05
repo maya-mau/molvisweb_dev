e00579bed652a6be838bb666a3c7b510cd0df5be
scoutdp <141675868+scoutdp@users.noreply.github.com>
2024-05-05 17:30:41 -0400

    Update interactive_text.js

--- interactive_text.js
+++ interactive_text.js
@@ -13,7 +13,7 @@ const div = document.getElementById('start');
 nextButton.addEventListener('click', nextDiv);
 backButton.addEventListener('click', backDiv); 
 
-var zero = "<h1>Welcome!</h1> ";
+var zero = "<h1>Welcome!</h1> <p> This is an in-progress website to bring VMD-like molecule interaction and exploration to a broader audience by making it in an html website. Try moving around the molecule, switching what is represented, and selecting atoms. This part of the page will eventually function to guide students through a set of activites related to the molecules. </p>";
 textArray.push(zero); 
 
 var one = "<p>In the prior activity, you read about Sandra, who has chronic myeloid leukemia (CML).  You learned about a drug, imatinib, that can be used to treat CML by binding to a protein target.  There are many drugs that can be used to treat CML and that can bind to this same protein. The specific way that drug molecules bind to the protein depends on the intermolecular forces (noncovalent interactions) between the molecules.  Today, you are going to explore the structure of another drug used to treat CML, called ponatinib. The structure of the drug molecule determines how it will interact with the protein target.</p>";
